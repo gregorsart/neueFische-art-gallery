0074e46ab3afe73c813b3121e644b54ac34cdd0a
Alina Rubart <rubartalina@gmail.com>
2023-09-12 17:28:29 +0800

    add fetch

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
+import useSWR from "swr";
+import Image from "next/image";
+
+const url = "https://example-apis.vercel.app/api/art";
+
 export default function HomePage() {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(url, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+  console.log(data);
+
+  // const { artist, name, imageSource } = data;
+
   return (
-    <div>
+    <ul>
       <h1>Hello from Next.js</h1>
-    </div>
+      {data.map((piece) => {
+        return (
+          <li key={piece.slug}>
+            {piece.artist}{" "}
+            <Image
+              src={piece.imageSource}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </li>
+        );
+      })}
+    </ul>
   );
 }
