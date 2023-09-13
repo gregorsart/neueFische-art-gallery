2c222da6be6b01dfea7a7d6ef56ddc315b7bcc0b
Achim Bartscht <beemerhh@gmail.com>
2023-09-13 11:16:42 +0200

    add navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Layout from "@/components/Layout";
 import GlobalStyle from "../styles";
 import useSWR from "swr";
 
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }) {
     <>
       <GlobalStyle />
       <Component {...pageProps} pieces={data} />
+      <Layout />
     </>
   );
 }
