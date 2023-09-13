979767e27064752c15f4e3bf258f8a9a36ba6b57
Achim Bartscht <beemerhh@gmail.com>
2023-09-13 11:35:16 +0200

    fix links

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,16 +2,18 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <ul>
-      <li>
-        <Link href={"/"}>Spotlight</Link>
-      </li>
-      <li>
-        <Link href={"/art-pieces"}>Art Pieces</Link>
-      </li>
-      <li>
-        <Link href={"/favorites"}>Favorites</Link>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Spotlight</Link>
+        </li>
+        <li>
+          <Link href="/art-pieces">Art Pieces</Link>
+        </li>
+        <li>
+          <Link href="/favorites">Favorites</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
