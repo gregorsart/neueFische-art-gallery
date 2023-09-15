32de6ab824ad78bd8bb6986e0866832852225868
Gregor Haslinger <kontakt@gregorsart.de>
2023-09-15 11:47:52 +0200

    feat (add favorites page with like toggle functionality)

--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -1,3 +1,37 @@
-export default function Favorites() {
-  return <div>Favorites</div>;
+import ArtPieces from "@/components/ArtPieces";
+export default function Favorites({ pieces, artPiecesInfo, onToggleFavorite }) {
+  // const favoritePiecesArray = artPiecesInfo.filter((piece) => {
+  //   return piece.isFavorite === true;
+  // });
+
+  // const container = [];
+  // favoritePiecesArray.forEach((element) => {
+  //   const fruit = pieces.find((info) => info.slug === element.slug);
+  //   if (fruit) container.push(fruit);
+  // });
+
+  // artPiecesInfo = [
+  //   { slug: "red-green-blue", isFavorite: false },
+  //   { slug: "red-purple-blue", isFavorite: true },
+  // ];
+
+  // Roland's Code // Solution
+  const container = pieces.filter((piece) => {
+    return artPiecesInfo.find((info) => info.slug === piece.slug)?.isFavorite;
+  });
+
+  // Alina's Code // works
+  // const containerTwo = pieces.filter((piece) => {
+  //   const buffer = artPiecesInfo.find((info) => info.slug === piece.slug);
+  //   if (buffer) return buffer;
+  //   else return null;
+  // });
+
+  return (
+    <ArtPieces
+      pieces={container}
+      artPiecesInfo={artPiecesInfo}
+      onToggleFavorite={onToggleFavorite}
+    />
+  );
 }
